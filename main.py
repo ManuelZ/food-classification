6bd3c1213a8b3c3a37636ace79599db4107dcc68
ManuelZ <115771+ManuelZ@users.noreply.github.com>
2026-06-25 20:05:41 -0500

    Add import comments

--- main.py
+++ main.py
@@ -3,8 +3,10 @@
 #   python main.py validate --config config.yaml --ckpt_path path/to/checkpoint.ckpt
 #   python main.py predict --config config.yaml --ckpt_path path/to/checkpoint.ckpt
 
+# External imports
 from lightning.pytorch.cli import LightningCLI
 
+# Local imports
 from datamodule import KenyaDataModule
 from model import FineTuningWithResNet
 
